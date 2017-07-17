2b58c0a957cc808dbfb5bff15da89b48483c0efe
Mike Fuller <mmfuller@impactanalysts.com>
2017-07-17 14:01:21 -0600

    Update Datum.cpp

--- Datum.cpp
+++ Datum.cpp
@@ -8,7 +8,7 @@
  *
  */
 
-#include "/Users/KarmaYeshe/Documents/ACADEMIC_PROJECTS/UTOR_Manuscripts_Projects/SORTIE_RELATED_PROJECTS/FULLER_CPP_PROGRAMS/DEVELOPMENT/General_Use_Classes/Classes_Datum/Datum.h"
+#include "Datum.h"
 namespace MMFdata
 {
 	
