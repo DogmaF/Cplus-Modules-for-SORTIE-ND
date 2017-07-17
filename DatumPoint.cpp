a2682d4e17e96113a95a47bf9b4266d924369f40
Mike Fuller <mmfuller@impactanalysts.com>
2017-07-17 14:01:50 -0600

    Update DatumPoint.cpp

--- DatumPoint.cpp
+++ DatumPoint.cpp
@@ -9,7 +9,7 @@
  */
 
 #include <iostream>
-#include "/Users/KarmaYeshe/Documents/ACADEMIC_PROJECTS/UTOR_Manuscripts_Projects/SORTIE_RELATED_PROJECTS/FULLER_CPP_PROGRAMS/DEVELOPMENT/General_Use_Classes/Classes_Datum/DatumPoint.h"
+#include "DatumPoint.h"
 using namespace std;
 namespace MMFdata
 {
